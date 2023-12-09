65e99707dc61f04b800785c457fb88d881eef14f
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 19:41:50 +0200

    Create lab2-ex5.py

---
+++ lab2-ex5.py
@@ -0,0 +1,6 @@
+# You obtained a secret message that was encrypted using Vigenère cipher with a simple pass phrase. Thus, it is vulnerable to frequency analysis. Decode this message and find out what does this message say and what is the encryption key.
+# Cipher message:
+# OOGNVMTNTCLUOGZSZSHTXAZGMOMEPKWDDQM
+
+
+  
