4bdea1f93cb7a3c2177b606fe07f460865b9a8cc
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 19:30:21 +0200

    Update lab1-ex2.py

--- lab1-ex2.py
+++ lab1-ex2.py
@@ -1,3 +1,7 @@
+# Create a program that would ask for two boolean values (true or false, 0 or 1) and would output the result for the XNOR operation performed on them.
+# You're allowed to use only `and`, `or` and `not` operations
+
+
 bool1 = bool(int(input("boolean 1:")))
 bool2 = bool(int(input("boolean 2:")))
 
