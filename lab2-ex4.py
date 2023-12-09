98d165f69ee0e6bd10f92504a5a683bfff7b1bd5
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 19:41:09 +0200

    Create lab2-ex4.py

---
+++ lab2-ex4.py
@@ -0,0 +1,3 @@
+# You are given a string str. You can convert str to a palindrome by adding characters in front of it. Return the shortest palindrome you can find by performing this transformation.
+
+
