0eed38ab2006fe03a9ca08ebce753c78d15bff3e
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 19:40:13 +0200

    Create lab2-ex3.py

---
+++ lab2-ex3.py
@@ -0,0 +1,12 @@
+# We all use many passwords every day but is every password we use hard enough to crack. Your password is considered strong if it follows this rules:
+# -It has at least 8 characters and at most 20 characters.
+# -It has a minimum of 1 lower case letter [a-z] and a minimum of 1 upper case letter [A-Z] and a minimum of 1 numeric character [0-9] and a minimum of 1 special character: ~`!@#$%^&*()-_+={}[]|\;:"<>,./?
+# -It does not contain three repeating characters in a row (i.e.,“abbbcc-0”is weak, but  is “ abcbbcc-0”strong).
+# - Shouldn’t contain any consecutive numbers
+# Given a string password, return the minimum number of steps required to make the password strong. If the password is already strong, return the message “good”.
+# In one step, you can:
+# -Insert one character
+# -Delete one character
+# -Replace one character
+
+
