9fb0b64bcb9299e45b0ae5435c8820d55bd3512f
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 19:53:45 +0200

    Create lab2-bonus.py

---
+++ lab2-bonus.py
@@ -0,0 +1,5 @@
+# In this problem we need to create our own Mastermind game. In our version of Mastermind game users can choose between two versions of the game: auto and manual. In the auto version the computer will randomly select a secret code and the user tries to guess it, based on the deterministic clues given by the computer and in the manual version the user will input the secret code and another user tries to guess it based on clues that appear on screen. You must make this game playable in the terminal.
+# Bonus task:
+# Make for your game a graphical interface
+
+
