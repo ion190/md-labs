bbc9d849e5329026f6bd9ec792604b0e01c96e3e
none190 <145373308+none190@users.noreply.github.com>
2023-12-17 20:57:30 +0200

    Update lab2-ex4.py

--- lab2-ex4.py
+++ lab2-ex4.py
@@ -1,3 +1,9 @@
 # You are given a string str. You can convert str to a palindrome by adding characters in front of it. Return the shortest palindrome you can find by performing this transformation.
 
-
+s = input("Enter string: ")
+# Find the longest palindrome
+for i in range(len(s), 0, -1):
+    if s[:i] == s[:i][::-1]:
+        # Add the remaining characters in reverse order to the beginning
+        print(s[i:][::-1] + s)
+        break
