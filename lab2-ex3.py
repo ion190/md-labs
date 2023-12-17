73c3b325c62f39e53b0070c6b042bcfa190a75ea
none190 <145373308+none190@users.noreply.github.com>
2023-12-17 20:57:11 +0200

    Update lab2-ex3.py

--- lab2-ex3.py
+++ lab2-ex3.py
@@ -9,4 +9,44 @@
 # -Delete one character
 # -Replace one character
 
+def steps_to_strong(password):
+    # Calculate the steps needed to make the password strong
+    steps = 0
+
+    # Add missing character types
+    if not any(c.islower() for c in password):
+        steps += 1
+    if not any(c.isupper() for c in password):
+        steps += 1
+    if not any(c.isdigit() for c in password):
+        steps += 1
+    if not any(c in '~`!@#$%^&*()-_+={}[]|;:"<>,./?' for c in password):
+        steps += 1
+
+    # Ensure the length is between 8 and 20
+    if len(password) < 8:
+        steps += 8 - len(password) - steps
+    elif len(password) > 20:
+        steps += len(password) - 20 - steps
+
+    # Remove repeating characters
+    for i in range(len(password) - 2):
+        if password[i] == password[i + 1] == password[i + 2]:
+            steps += 1
+
+    # Avoid consecutive numbers
+    for i in range(len(password) - 1):
+        if password[i].isdigit() and password[i + 1].isdigit() and abs(
+                int(password[i]) - int(password[i + 1])) == 1:
+            steps += 1
+
+    return steps
+
+
+password = str(input('Input password: '))
+steps = steps_to_strong(password)
+if steps == 0:
+    print('Good password')
+else:
+    print('Steps required to make the password strong: ', steps)
 
