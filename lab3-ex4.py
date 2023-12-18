05b7ac1d78d70cb54c88b0fb7e46ddfca7f805db
none190 <145373308+none190@users.noreply.github.com>
2023-12-18 21:33:06 +0200

    Update lab3-ex4.py

--- lab3-ex4.py
+++ lab3-ex4.py
@@ -17,17 +17,15 @@ from string import punctuation
 from collections import Counter
 from nltk.tokenize import word_tokenize
 
-
-def writeinfile(file_path,tweets,emotions):
+def writeinfile(file_path, tweets, emotions):
     with open(file_path, 'w') as file:
         for i in range(len(tweets)):
             file.write("{0} : {1:.2f}\n".format(tweets[i]['id'],emotions[i]))
 
-
-def replace_punct(text,set):
+def replace_punct(text, set):
     for i in range(len(text)):
         if text[i] in set:
-            text=text.replace(f'{text[i]}',' ')
+            text = text.replace(f'{text[i]}', ' ')
     return text
 
 def read_json(file_path):
@@ -42,11 +40,11 @@ def read_emotions(file_path):
         lines[i]=lines[i].strip().split()
 
     dict = {' '.join(line[0:len(line)-1]): int(line[-1]) for line in lines}
-
     return dict
 
-#PROBLEM 4.1
-print("PROBLEM 4.1")
+
+
+print("Problem 4.1\n10 most popular #hashtags followed by the number of occurrences of the #hashtag")
 tweets=read_json("tweets.json")
 hashtags=[]
 spec_characters=set(punctuation)
@@ -62,19 +60,19 @@ for tweet in tweets:
         if word[0]=='#' and len(word)>1 and word[1].isalpha():
             hashtags.append(word)
 
-
 count_hashtags= Counter(hashtags)
 sorted_hashtags=dict(sorted(count_hashtags.items(), key=lambda item: item[1],reverse=True))
 i=0
 for hashtag,count in sorted_hashtags.items():
-    print("{0}. {1} : {2}".format(i+1,hashtag,count))
+    print("{0}. {1}: {2}".format(i+1,hashtag,count))
     i+=1
     if i==10:
         break
 
 
-#PROBLEM 4.2
-print("\n\nPROBLEM 4.2")
+
+
+print("\n\nProblem 4.2\nfind the emotional value for every tweet")
 emotions=read_emotions("AFINN-111.txt")
 emotional_value=[]
 
@@ -91,30 +89,29 @@ for sentence in sentences:
     else:
         emotional_value.append(0)
 
-writeinfile("output.txt",tweets,emotional_value)
-print("The data was sent in 'output.txt'")
-#PROBLEM 4.3
+writeinfile("output.txt", tweets, emotional_value)
+print("Data is stored in 'output.txt'")
 
-print("\n\nPROBLEM 4.3")
+
+
+
+print("\n\nProblem 4.3\n10 most positive tweets and 10 most negative tweets")
 for i in range(len(tweets)):
     tweets[i]['emotional_value']=emotional_value[i]
 sorted_tweets = sorted(tweets, key=lambda x: x['emotional_value'])
 
+print("\nMost Positive Tweets")
 i=0
-print("Most Negative Tweets")
-for tweet in sorted_tweets:
-    print(i+1," : ", tweet['text'])
-    i+=1
-    if i==10:
-        break
-
-print("Most Positive Tweets")
-i=0
-
 for tweet in sorted_tweets[::-1]:
     print(i+1," : ", tweet['text'])
     i+=1
     if i==10:
         break
-print()
-print()
+
+print("Most Negative Tweets")
+i=0
+for tweet in sorted_tweets:
+    print(i+1," : ", tweet['text'])
+    i+=1
+    if i==10:
+        break
